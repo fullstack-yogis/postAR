ca56d1a93caa4829f20416334a0607b97325a6da
Tony Wong <wongtonyb@gmail.com>
2019-10-07 17:08:53 -0400

    organized app.js & home.js

--- App.js
+++ App.js
@@ -1,103 +1,41 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableHighlight, StyleSheet } from 'react-native';
-// import { APP_SECRET } from './front_secrets';
 
+// Components
+import Home from './components/Home';
 import LogIn from './components/LogIn';
 import SignUp from './components/SignUp';
 
-/*
- TODO: Insert your API key below
- */
-// var sharedProps = {
-//   apiKey: APP_SECRET,
-// };
+// View Types for state
+let HOME_VIEW = 'HOME';
+let LOGIN_VIEW = 'LOGIN';
+let SIGN_UP_VIEW = 'SIGNUP';
 
-let UNSET = 'UNSET';
-let LOGIN_TYPE = 'LOGIN';
-let SIGN_UP_TYPE = 'SIGNUP';
-
-let defaultNavigatorType = UNSET;
+let defaultScreenView = HOME_VIEW;
 
 export default class postAR extends Component {
   constructor() {
     super();
     this.state = {
-      navigatorType: defaultNavigatorType,
-      // sharedProps: sharedProps,
+      screenView: defaultScreenView,
     };
-    this.displayHome = this.displayHome.bind(this);
-    this.displayLogIn = this.displayLogIn.bind(this);
-    this.displaySignUp = this.displaySignUp.bind(this);
-    this.changeNavigatorType = this.changeNavigatorType.bind(this);
+    this.changeScreenView = this.changeScreenView.bind(this);
   }
 
-  render() {
-    if (this.state.navigatorType === UNSET) {
-      return this.displayHome();
-    } else if (this.state.navigatorType === LOGIN_TYPE) {
-      return this.displayLogIn();
-    } else if (this.state.navigatorType === SIGN_UP_TYPE) {
-      return this.displaySignUp();
-    }
-  }
-
-  displayHome() {
-    return (
-      <View style={styles.container}>
-        <Text>postAR</Text>
-        <View>
-          <TouchableHighlight
-            style={styles.buttons}
-            // underlayColor={'#68a0ff'}
-            onPress={this.changeNavigatorType(LOGIN_TYPE)}
-          >
-            <Text>Login</Text>
-          </TouchableHighlight>
-          <TouchableHighlight
-            style={styles.buttons}
-            // underlayColor={'#68a0ff'}
-            onPress={this.changeNavigatorType(SIGN_UP_TYPE)}
-          >
-            <Text>Sign Up</Text>
-          </TouchableHighlight>
-        </View>
-      </View>
-    );
-  }
-
-  changeNavigatorType(navigatorType) {
+  changeScreenView(screenView) {
     return () => {
       this.setState({
-        navigatorType: navigatorType,
+        screenView: screenView,
       });
     };
   }
 
-  displayLogIn() {
-    return <LogIn />;
-  }
-
-  displaySignUp() {
-    return <SignUp />;
+  render() {
+    if (this.state.screenView === HOME_VIEW) {
+      return <Home changeScreenView={this.changeScreenView} />;
+    } else if (this.state.screenView === LOGIN_VIEW) {
+      return <LogIn />;
+    } else if (this.state.screenView === SIGN_UP_VIEW) {
+      return <SignUp />;
+    }
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-  },
-  buttons: {
-    height: 80,
-    width: 150,
-    paddingTop: 20,
-    paddingBottom: 20,
-    marginTop: 10,
-    marginBottom: 10,
-    backgroundColor: '#68a0cf',
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: '#fff',
-  },
-});
--- components/Home.js
+++ components/Home.js
@@ -1,18 +1,31 @@
-import React, { Component } from 'react';
-import { StyleSheet, View, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { View, Text, TouchableHighlight, StyleSheet } from 'react-native';
 
-export default class Home extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>postAR</Text>
-        <View>
-          <TouchableOpacity>Login</TouchableOpacity>
-          <TouchableOpacity>Sign Up</TouchableOpacity>
-        </View>
+let LOGIN_VIEW = 'LOGIN';
+let SIGN_UP_VIEW = 'SIGNUP';
+
+export function Home(props) {
+  return (
+    <View style={styles.container}>
+      <Text>postAR</Text>
+      <View>
+        <TouchableHighlight
+          style={styles.buttons}
+          underlayColor="#68a0ff"
+          onPress={props.changeScreenView(LOGIN_VIEW)}
+        >
+          <Text>Login</Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={styles.buttons}
+          underlayColor="#68a0ff"
+          onPress={props.changeScreenView(SIGN_UP_VIEW)}
+        >
+          <Text>Sign Up</Text>
+        </TouchableHighlight>
       </View>
-    );
-  }
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -21,4 +34,18 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'center',
   },
+  buttons: {
+    height: 80,
+    width: 150,
+    paddingTop: 20,
+    paddingBottom: 20,
+    marginTop: 10,
+    marginBottom: 10,
+    backgroundColor: '#68a0cf',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#fff',
+  },
 });
+
+export default Home;
