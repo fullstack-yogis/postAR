e5e22ff8d1366dc2c24079a63aae4804b1d0813a
Helen Qiong Zhang <helenqiongzhang@gmail.com>
2019-10-11 12:10:49 -0400

    a better version of connect apollo client

--- Components/AllPosts.js
+++ Components/AllPosts.js
@@ -3,9 +3,9 @@ import { Text, View } from 'react-native';
 import SinglePost from './SinglePost';
 import CreatePost from './CreatePost';
 import gql from 'graphql-tag';
-import { Query } from 'react-apollo';
+import { withApollo } from 'react-apollo';
 import { specifiedRules } from 'graphql';
-import { client } from '../index.ios';
+// import { client } from '../index.ios';
 
 const FEED_QUERY = gql`
   {
@@ -27,7 +27,7 @@ class AllPosts extends Component {
 
   async componentDidMount() {
     try {
-      const { data } = await client.query({
+      const { data } = await this.props.client.query({
         query: FEED_QUERY,
       });
       console.log('data in componentdidmount:', data);
@@ -42,22 +42,6 @@ class AllPosts extends Component {
   render() {
     return (
       <View>
-        {/* <Query query={FEED_QUERY}>
-          {({ loading, error, data }) => {
-            if (loading) return <Text>Fetching</Text>;
-            if (error) return <Text>Error</Text>;
-
-            const postsToRender = data.feed;
-
-            return (
-              <View>
-                {postsToRender.map(post => (
-                  <SinglePost key={post.id} post={post} />
-                ))}
-              </View>
-            );
-          }}
-        </Query> */}
         <View>
           {this.state.posts.map(post => (
             <SinglePost key={post.id} post={post} />
@@ -69,4 +53,4 @@ class AllPosts extends Component {
   }
 }
 
-export default AllPosts;
+export default withApollo(AllPosts);
